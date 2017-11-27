473e4914f8a17cfde78ff570f2da8ad59cd0ce4d
Adam McArthur <adam@adammcarthur.net>
2017-11-28 02:59:57 +1100

    Add message send functionality

--- index.js
+++ index.js
@@ -11,22 +11,21 @@ if (!fs.existsSync(path.join(__dirname, "./config/_sent.json"))) {
 }
 
 let sent = require(path.join(__dirname, "./config/_sent.json")).sent;
-
-//login({email: fbConfig.email, password: fbConfig.passsword}, (err, api) => {
-  //if (err) return console.error(err);
+login({email: fbConfig.email, password: fbConfig.password}, (err, api) => {
+  if (err) return console.error(err);
 
   messageToSend = phrases[0];
   console.log("Sending: " + messageToSend);
  
-  // fbConfig.recipients.forEach((id) => {
-  //   api.sendMessage(messageToSend, id);
-  // });
+  fbConfig.recipients.forEach((id) => {
+    api.sendMessage(messageToSend, id);
+  });
 
-  // Update the data lists before closing this session.
+  Update the data lists before closing this session.
   sent.push(messageToSend);
   i = phrases.indexOf(messageToSend);
   phrases.splice(i, 1);
 
   fs.writeFileSync(path.join(__dirname, "./config/phrases.json"), JSON.stringify({phrases: phrases}));
   fs.writeFileSync(path.join(__dirname, "./config/_sent.json"), JSON.stringify({sent: sent}));
-//});
+});
